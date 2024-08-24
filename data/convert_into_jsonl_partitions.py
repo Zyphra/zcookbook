72b35ca76c7d797b8e483500d57f468111206ac0
Yury Tokpanov <yury@zyphra.com>
2024-08-24 00:25:32 +0000

    Fix args

--- data/convert_into_jsonl_partitions.py
+++ data/convert_into_jsonl_partitions.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     parser.add_argument("--hf-dir", type=str, default=None, help="Dir in HF dataset")
     parser.add_argument("--save-path", type=str, required=True, help="Folder to save partitioned JSONL dataset to")
     parser.add_argument("--num-proc", type=int, default=1, help="Number of processes for HF processing")
-    parser.add_argument("--num_partitions", type=int, default=1, help="Number of partitions to split the dataset into")
+    parser.add_argument("--num-partitions", type=int, default=1, help="Number of partitions to split the dataset into")
     args = parser.parse_args()
 
     logging.info("Loading the dataset")
@@ -34,11 +34,11 @@ if __name__ == "__main__":
     )
 
     logging.info("Saving JSONL partitions")
-    n_proc = min(args.n_proc, args.partitions)
-    inds_distr = more_itertools.distribute(n_proc, range(args.partitions))
+    n_proc = min(args.num_proc, args.num_partitions)
+    inds_distr = more_itertools.distribute(n_proc, range(args.num_partitions))
     processes = []
     for process_inds in inds_distr:
-        p = mp.Process(target=save_shard, args=(ds, args.save_path, args.partitions, process_inds))
+        p = mp.Process(target=save_shard, args=(ds, args.save_path, args.num_partitions, process_inds))
         processes.append(p)
         p.start()
     for p in processes:
